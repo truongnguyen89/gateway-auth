73a13dd65e3fc5033744c46ac163958c35c50ca5
Nguyen Quang Truong <truongnguyen89@gmail.com>
2018-12-26 14:27:06 +0700

    @EnableZuulProxy
    @EnableZuulProxy and delete target folder

--- src/main/java/com/football/gateway/GatewayAuthApplication.java
+++ src/main/java/com/football/gateway/GatewayAuthApplication.java
@@ -4,9 +4,11 @@ import com.football.gateway.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableConfigurationProperties(AppProperties.class)
+@EnableZuulProxy
 public class GatewayAuthApplication {
 
     public static void main(String[] args) {
