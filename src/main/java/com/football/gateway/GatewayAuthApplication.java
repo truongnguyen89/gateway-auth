f1ca25e7a9d5ee11fb1471783b5061236d2a7f7a
Nguyen Quang Truong <truongnguyen89@gmail.com>
2018-12-28 15:35:37 +0700

    fix ok
    add config
    zuul:
        ignoredPatterns:
            - /auth/login/**
            - /auth/signup/**
            - /user/me

--- src/main/java/com/football/gateway/GatewayAuthApplication.java
+++ src/main/java/com/football/gateway/GatewayAuthApplication.java
@@ -1,15 +1,43 @@
 package com.football.gateway;
 
 import com.football.gateway.config.AppProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
 
 @SpringBootApplication
 @EnableConfigurationProperties(AppProperties.class)
+@EnableZuulProxy
 public class GatewayAuthApplication {
+    private static final Logger LOGGER = LoggerFactory.getLogger(GatewayAuthApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(GatewayAuthApplication.class, args);
+        long id = System.currentTimeMillis();
+        LOGGER.info("[B][" + id + "] >>>>>>>>>>>>>>>>>>>>>>>>>> Start GatewayAuthApplication ...");
+        SpringApplication app = new SpringApplication(GatewayAuthApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        String protocol = "http";
+        if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
+        }
+        String ipServer = "localhost";
+        try {
+            ipServer = InetAddress.getLocalHost().getHostAddress();
+        } catch (Exception e) {
+            ipServer = env.getProperty("server.address") != null ? env.getProperty("server.address") : "localhost";
+        }
+        LOGGER.info("----------------------------------------------------------");
+        LOGGER.info("   Application         : " + env.getProperty("spring.application.name"));
+        LOGGER.info("   Url                 : " + protocol + "://" + ipServer + ":" + env.getProperty("server.port") + "/api/**");
+        LOGGER.info("   Profile(s)          : " + env.getActiveProfiles()[0]);
+        LOGGER.info("----------------------------------------------------------");
+
+        LOGGER.info("[E][" + id + "][Duration = " + (System.currentTimeMillis() - id) + "] >>>>>>>>>>>>>>>>>>>>>>>>>> SUCCESS <<<<<<<<<<<<<<<<<<<<<<<<<");
     }
 }
